64926999c5555df0dc782790bc382daad29685bc
steve <stevens714@yahoo.com>
2020-10-12 15:44:20 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,34 @@
-# Your code goes here!
+# class Anagram
+#     attr_accessor :word
+
+#     def intialize(word)
+#         @word = word
+#     end
+
+#     def match(string)
+#         string.select do |w|
+#             w.split("").sort == word.split("").sort
+#         end
+#     end
+# end
+
+class Anagram
+    def initialize(anagram)
+      @anagram = anagram
+    end
+  
+    def match(word_list)
+      anagrams = []
+      word_list.each do |word|
+        if prep(@anagram) == prep(word) && @anagram.downcase != word.downcase
+          anagrams << word
+        end
+      end
+      anagrams
+    end
+  
+    private
+    def prep(word)
+      word.downcase.scan(/\w/).sort
+    end
+  end
